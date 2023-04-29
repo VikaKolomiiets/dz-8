e3c959a31e51e2bda244df9436aa44f323f2a426
Vika Kolomiets <vikakolomiets@gmail.com>
2023-04-29 19:16:23 +0300

    chore: update methods in Group

--- homeTask_8/src/Group.java
+++ homeTask_8/src/Group.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class Group {
     private Student groupLeader;
     private List<Student> students = new ArrayList<Student>();
-    private List tasks = new ArrayList<Task>();
+    private List<Task> tasks = new ArrayList<Task>();
 
     public Group(Student groupLeader) throws Exception {
         if (groupLeader == null){
@@ -28,28 +28,31 @@ public class Group {
         this.students.add(groupLeader);
         this.groupLeader = groupLeader;
     }
-
     public Student getGroupLeader() {
         return groupLeader;
     }
-
-    public List getTasks() {
-        return tasks;
+    public List<Task> getTasks() {
+        return this.tasks;
     }
-
 
     public void changeGroupLeader(Student newGroupLeader) throws Exception {
         if (newGroupLeader == null){
             throw new Exception("Student can not be the null");
         }
-        //todo check for present in group, and what to do?
+        if (!this.students.contains(newGroupLeader)){
+            throw new Exception("Add new Group Leader to this Group");
+        }
         this.groupLeader = newGroupLeader;
     }
     public void addStudent(Student student) throws Exception {
         if (student == null) {
             throw new Exception("Student can not be the null");
         }
-        //Check Id
+        for(Student item: this.students){
+            if(item.getId() == student.getId()){
+                throw new Exception("Student with same Id is existed in the Group");
+            }
+        }
         this.students.add(student);
     }
     public void removeStudent(Student student) throws Exception {
@@ -59,7 +62,9 @@ public class Group {
         if (!this.students.contains(student)){
             throw new Exception("This student does not belong to this group");
         }
-        //Check Student != Leader
+        if (student.getId() == this.groupLeader.getId()){
+            throw new Exception("This Student is the Group Leader, you should change Group Leader before removing current Student");
+        }
         this.students.remove(student);
     }
     public Student renameStudent(UUID studentId, String firstName, String lastName) throws Exception {
@@ -70,17 +75,12 @@ public class Group {
                 return student;
             }
         }
-        throw new Exception();
+        throw new Exception("Student with this Id does not exist in this group");
     }
-    public void addTaskForGroup(String task) throws Exception {
+    public void addTaskForGroup(Task task) throws Exception {
         if (task == null) {
             throw new Exception("Task can not be a null");
         }
         this.tasks.add(task);
     }
-
-
-
-
-
 }
